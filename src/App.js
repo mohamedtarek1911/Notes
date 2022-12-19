d347dfe15ed9166808f04f28162607a0976f197a
mohamedtarek1911 <mohamedtarek7670@gmail.com>
2022-12-19 21:22:36 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import Register from "./Components/Register";
 import Login from "./Components/Login";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createHashRouter,
+} from "react-router-dom";
 import MainLAyout from "./Components/MainLAyout";
 import Home from "./Components/Home";
 import Particles from "./Components/Particles";
 import ParticlesComponent from "./Components/Particles";
 
 function App() {
-  const routers = createBrowserRouter([
+  const routers = createHashRouter([
     {
       path: "/",
       element: <MainLAyout />,
